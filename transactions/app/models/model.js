966750bffc694fc33aad67b3eb93dcde8fad7d46
Rory O'Hare <roryeoinohare@gmail.com>
2024-03-11 10:10:38 +0000

    Fixed mixed up model.js files

--- transactions/app/models/model.js
+++ transactions/app/models/model.js
@@ -1,17 +1,42 @@
 const mongoose = require('mongoose');
 
-// create a mongoose schema for a quotation
-const balanceSchema = mongoose.Schema({
+const userSchema = mongoose.Schema({
     userId: {
         type: String,
         required: true,
         match: [/^\d{8}$/, 'User ID must consist of 8 numbers']
     },
-    balance: {
-        type: Number,
+    password: {
+        type: String,
         required: true,
     }
 });
 
-// export the model to our app
-module.exports = mongoose.model('Balance', balanceSchema);
+
+const transactionSchema = mongoose.Schema({
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    transactionType: {
+        type: String,
+        enum: ['debit', 'credit'],
+        required: true
+    },
+    amount: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    description: {
+        type: String,
+        required: false
+    }
+});
+
+module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('Transaction', transactionSchema);
--- transfer_money/app/models/model.js
+++ transfer_money/app/models/model.js
@@ -1,42 +1,17 @@
 const mongoose = require('mongoose');
 
-const userSchema = mongoose.Schema({
+// create a mongoose schema for a quotation
+const balanceSchema = mongoose.Schema({
     userId: {
         type: String,
         required: true,
         match: [/^\d{8}$/, 'User ID must consist of 8 numbers']
     },
-    password: {
-        type: String,
-        required: true,
-    }
-});
-
-
-const transactionSchema = mongoose.Schema({
-    userId: {
-        type: Schema.Types.ObjectId,
-        ref: 'User'
-    },
-    date: {
-        type: Date,
-        default: Date.now
-    },
-    transactionType: {
-        type: String,
-        enum: ['debit', 'credit'],
-        required: true
-    },
-    amount: {
+    balance: {
         type: Number,
         required: true,
-        min: 0
-    },
-    description: {
-        type: String,
-        required: false
     }
 });
 
-module.exports = mongoose.model('User', userSchema);
-module.exports = mongoose.model('Transaction', transactionSchema);
+// export the model to our app
+module.exports = mongoose.model('Balance', balanceSchema);
